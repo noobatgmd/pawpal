1299bc9aa0b235ddd51eb91ec005869f27958111
noobatgmd <jason.cw.hong@gmail.com>
2025-08-09 05:21:46 +0800

    Update docs folder with latest web build

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -51,7 +51,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "ca11260cbbf8756100ebe3538ee65a9f
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "4af50de9ac2f77df4c49238d00d8abca",
+"flutter_bootstrap.js": "853b813a5fe9bb32ed863ce64fe8711e",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
